5756fac14bca23f6c5d0539ca0477e244906e427
Jamie Bland <jamie@theblands.net>
2023-12-13 02:24:28 -0600

    Refactor main.rs: Add command line arguments and functions for generating and cracking encrypted files

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,105 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-
-use std::collections::HashMap;
-use std::env;
-use std::fs::File;
-use std::io::Read;
+use rand::Rng;
 
 mod s_des;
-mod entropy;
 mod file_io;
+mod char_freq_analysis;
 
-use s_des::{gen_keys, gen_subkeys, encrypt, decrypt, Key};
-use entropy::calc_entropy;
-use file_io::{read_file_to_bytes, write_bytes_to_file};
+use crate::s_des::{gen_keys, encrypt, decrypt, Key};
+use crate::file_io::{read_file_to_bytes, write_bytes_to_file};
+use crate::char_freq_analysis::{
+    freq_file_to_hashmap,
+    calc_char_freq,
+    calc_euclidean_dist
+};
 
 fn main() {
-    // let keys = gen_keys();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        match args[1].as_str() {
+            "gen" => gen_test_file("plaintext"),
+            "crack" => crack_file("encrypted"),
+            _ => print_usage(),
+        }
+    } else if args.len() == 3 {
+        match args[1].as_str() {
+            "gen" => gen_test_file(&args[2]),
+            "crack" => crack_file(&args[2]),
+            _ => print_usage(),
+        }
+    } else {
+        print_usage();
+    }
+}
 
-    // for key in &keys {
-    //     println!("k1: {:#010b}, k2: {:#010b}", key.k1, key.k2);
-    // }
+fn print_usage() {
+    println!("Usage:\tcargo run [gen|crack] <filename>");
+    println!();
+    println!("\tgen:\tgenerates an encrypted file with a random key");
+    println!("\t\tfilename defaults to plaintext");
+    println!("\tcrack:\tcracks an encrypted file with brute force");
+    println!("\t\tfilename defaults to encrypted");
+}
 
-    // println!("Total keys generated: {}", keys.len());
+// Generates an encrypted file with a random key
+fn gen_test_file(file_name: &str) {
+    let file_path = format!("data/{}.txt", file_name);
 
-    // let my_key = gen_subkeys(0b1101001101).unwrap();
+    println!("Generating test file");
 
-    // println!("My key:");
-    // println!("k1: {:#010b}, k2: {:#010b}", my_key.k1, my_key.k2);
+    // Generate random key
+    let mut rng = rand::thread_rng();
+    let key = rng.gen_range(0..1024);
+    let key = s_des::gen_subkeys(key).unwrap();
+    println!("Key:\n\tk1: {:#010b}, \n\tk2: {:#010b}", key.k1, key.k2);
 
-    // Test encryption
-    let byte_to_encrypt = 0b10101010;
-    println!("plain byte: {:#010b}", byte_to_encrypt);
+    let input_data = read_file_to_bytes(&file_path);
+    println!("{} bytes read from {}", input_data.len(), file_path);
 
-    let my_key = Key {
-        k1: 0b11011010,
-        k2: 0b10001101,
-    };
+    let encrypted_data = encrypt(&input_data, &key);
 
-    let encrypted = s_des::encrypt(&[byte_to_encrypt], &my_key);
-    println!("Encrypted:  {:#010b}", encrypted[0]);
+    let file_path = "data/encrypted.txt";
+    write_bytes_to_file(file_path, &encrypted_data);
+    println!("{} bytes saved to {}", encrypted_data.len(), file_path);
+}
 
-    let decrypted = s_des::decrypt(&encrypted, &my_key);
-    println!("Decrypted:  {:#010b}", decrypted[0]);
+// Cracks an encrypted file with brute force
+fn crack_file(file_name: &str) {
+    let file_path = format!("data/{}.txt", file_name);
 
-    println!("Alrighty, that worked! Now let's encrypt and decrypt a file. ");
+    println!("Cracking file");
 
-    // Load input_test.txt in as vector of bytes
-    let input_file_path = "input_test.txt";
-    let input_data = read_file_to_bytes(input_file_path).unwrap();
-    println!("Read {} bytes from {}", input_data.len(), input_file_path);
+    let input_data = read_file_to_bytes(&file_path);
+    println!("{} bytes read from {}", input_data.len(), file_path);
 
-    // Encrypt the data
-    let encrypted_data = encrypt(&input_data, &my_key);
-    println!("Encrypted {} bytes", encrypted_data.len());
+    // Prepare character frequency of English from Shakespeare
+    let shakespeare_file_path = "resources/shakespeare_freq.txt";
+    let shakespeare_char_freq = freq_file_to_hashmap(shakespeare_file_path);
 
-    // Calculate the entropy of the encrypted data
-    let entropy = calc_entropy(&encrypted_data);
-    println!("Entropy: {}", entropy);
+    let keys = gen_keys();
 
-    // Write the encrypted data to a file
-    let encrypted_file_path = "encrypted_test.txt";
-    write_bytes_to_file(encrypted_file_path, &encrypted_data).unwrap();
+    let mut lowest_distance: Option<f32> = None;
+    let mut best_key: Option<&Key> = None;
+    for key in &keys {
+        let decrypted_data = decrypt(&input_data, key);
 
-    // Decrypt the data
-    let decrypted_data = decrypt(&encrypted_data, &my_key);
-    println!("Decrypted {} bytes", decrypted_data.len());
+        let char_freq = calc_char_freq(&decrypted_data);
 
-    // Calculate the entropy of the decrypted data
-    let entropy = calc_entropy(&decrypted_data);
-    println!("Entropy: {}", entropy);
+        let distance = calc_euclidean_dist(&char_freq, &shakespeare_char_freq);
 
-    // Write the decrypted data to a file
-    let decrypted_file_path = "decrypted_test.txt";
-    write_bytes_to_file(decrypted_file_path, &decrypted_data).unwrap();
+        if lowest_distance.is_none() || distance < lowest_distance.unwrap() {
+            lowest_distance = Some(distance);
+            best_key = Some(key);
+        }
+    }
+
+    let best_key = best_key.unwrap();
+
+    println!("Best key:");
+    println!("\tk1: {:010b}, \n\tk2: {:010b}", best_key.k1, best_key.k2);
+
+    let decrypted_data = decrypt(&input_data, best_key);
+
+    let output_file_path = "data/decrypted.txt";
+    write_bytes_to_file(output_file_path, &decrypted_data);
+    println!("{} bytes saved to {}", decrypted_data.len(), output_file_path);
 }
