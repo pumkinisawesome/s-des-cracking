878e9ec6477363144095efe7a5d6af6323f3508c
Jamie Bland <jamie@theblands.net>
2023-12-13 02:23:04 -0600

    Replaced entropy checking with character frequency analysis

--- docs/pseudocode_plan.md
+++ docs/pseudocode_plan.md
@@ -9,8 +9,11 @@
    2. ~~Generate subkeys from key~~
 3. For each key in the range:
     1. Decrypt the file using the key
-    2. Calculate the information entropy of the decrypted file
-    3. If the calculated entropy is lower than the current lowest entropy:
-       1. Update the lowest entropy
+    2. Calculate the frequency of each byte in the decrypted file
+    3. Calculate the euclidean distance between the frequency of each byte in
+        the decrypted file and the frequency of each byte in the English
+        language
+    4. If the euclidean distance is the lowest so far:
+       1. Update the lowest euclidean distance
        2. Update the best key to the current key
 4. Output the best key and the decrypted file
---
+++ src/char_freq_analysis.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+use super::file_io::read_file_to_string;
+
+pub fn calc_char_freq(data: &Vec<u8>) -> HashMap<u8, f32> {
+    let mut freq_map = HashMap::new();
+
+    // Iterate through bytes, and save frequency of byte as fraction of total
+    for &byte in data {
+        *freq_map.entry(byte).or_insert(0.0) += 1.0;
+    }
+
+    // Convert frequency to fraction of total
+    for byte_freq in freq_map.values_mut() {
+        *byte_freq /= data.len() as f32;
+    }
+
+    freq_map
+}
+
+pub fn calc_euclidean_dist(freq1: &HashMap<u8, f32>, freq2: &HashMap<u8, f32>) -> f32 {
+    let mut sum = 0.0;
+
+    // Calculate euclidean distance for all bytes in freq1
+    for (&byte, &freq) in freq1.iter() {
+        let diff = freq - freq2.get(&byte).unwrap_or(&0.0);
+        sum += diff * diff;
+    }
+
+    // Calculate euclidean distance for all bytes in freq2 that are not in freq1
+    for (&byte, &freq) in freq2.iter() {
+        if !freq1.contains_key(&byte) {
+            sum += freq * freq;
+        }
+    }
+
+    sum.sqrt()
+}
+
+pub fn freq_file_to_hashmap(file_path: &str) -> HashMap<u8, f32> {
+    let contents = read_file_to_string(file_path);
+
+    let mut freq_map = HashMap::new();
+
+    // Iterate through lines, and save frequency of byte as fraction of total
+    for line in contents.lines() {
+        let parts: Vec<&str> = line.split(':').collect();
+        let byte = parts[0].parse::<u8>().unwrap();
+        let freq = parts[1].parse::<f32>().unwrap();
+        freq_map.insert(byte, freq);
+    }
+
+    freq_map
+}
