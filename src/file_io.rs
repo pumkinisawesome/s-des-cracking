de2d8133ef4e6878920539eed81430df31ddd973
Jamie Bland <jamie@theblands.net>
2023-12-13 02:24:09 -0600

    Updated file io to add reading as string

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,12 +1,34 @@
 use std::fs::File;
-use std::io::{Read, Write, Result};
+use std::io::{Read, Write};
 use std::process::exit;
 
-pub fn read_file_to_bytes(file_path: &str) -> Result<Vec<u8>> {
+pub fn read_file_to_string(file_path: &str) -> String {
     let mut file = match File::open(file_path) {
         Ok(file) => file,
         Err(e) => {
-            eprintln!("Error opening file: {}", e);
+            eprintln!("Error opening file {file_path}: {}", e);
+            exit(1);
+        }
+    };
+
+    // read the file into a string
+    let mut data = String::new();
+    match file.read_to_string(&mut data) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error reading file {file_path}: {}", e);
+            exit(1);
+        }
+    };
+
+    data
+}
+
+pub fn read_file_to_bytes(file_path: &str) -> Vec<u8> {
+    let mut file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening file {file_path}: {}", e);
             exit(1);
         }
     };
@@ -16,15 +38,15 @@ pub fn read_file_to_bytes(file_path: &str) -> Result<Vec<u8>> {
     match file.read_to_end(&mut data) {
         Ok(_) => (),
         Err(e) => {
-            eprintln!("Error reading file: {}", e);
+            eprintln!("Error reading file {file_path}: {}", e);
             exit(1);
         }
     };
 
-    Ok(data)
+    data
 }
 
-pub fn write_bytes_to_file(file_path: &str, data: &[u8]) -> Result<()> {
+pub fn write_bytes_to_file(file_path: &str, data: &[u8]) {
     let mut output_file = match File::create(file_path) {
         Ok(file) => file,
         Err(e) => {
@@ -39,6 +61,4 @@ pub fn write_bytes_to_file(file_path: &str, data: &[u8]) -> Result<()> {
             exit(1);
         }
     };
-
-    Ok(())
 }
